da7ee1515e1b85b73016715b0c767182a4b5a65d
Prabakar <karan1690@ymail.com>
2017-08-28 11:09:07 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/trial.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'trial.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
