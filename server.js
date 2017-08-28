89e81536dd6f1d0e9826202392a17fe41358a760
Prabakar <karan1690@ymail.com>
2017-08-28 11:07:12 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/trial.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'trial.html'));
 });
 
